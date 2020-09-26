24e3db4ac40046f5734797949e115333c7d57725
Chanel Matlock <github@violetspark.net>
2020-09-25 17:30:40 -0700

    dropdown lists, resume and linkedIn links, and additional styling

--- js/violetSpark.js
+++ js/violetSpark.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+var dropdown_lists = document.querySelectorAll(".dropdown");
+
+dropdown_lists.forEach(function(currentValue){
+  currentValue.addEventListener("click", function(){
+    currentValue.classList.toggle("inactive");
+    currentValue.classList.toggle("active");
+  });
+});
